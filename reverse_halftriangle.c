e1640e15781d3c3d1ffacc515cc860f2ad8fbaa1
joemathew-it19 <53465666+joemathew-it19@users.noreply.github.com>
2019-10-21 15:11:32 +0530

    first commit

---
+++ reverse_halftriangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int i,j,k;
+    for(i=5;i>=1;i--)
+    {
+        for(j=5;j>i;j--)
+        {
+            printf(" ");
+        }
+    for(k=1;k<=i;k++)
+    {
+        printf("*");
+    }
+    printf("\n");
+    }
+}
