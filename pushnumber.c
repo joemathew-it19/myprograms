14c18146f4b6e02a18f281e88f3f8ff745c38375
joemathew-it19 <53465666+joemathew-it19@users.noreply.github.com>
2019-10-21 15:00:53 +0530

    first commit

---
+++ pushnumber.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int array[100], position, i, n, value;
+ 
+   printf("Enter number of elements in array\n");
+   scanf("%d", &n);
+ 
+   printf("Enter %d elements\n", n);
+ 
+   for (i = 0; i < n; i++)
+      scanf("%d", &array[i]);
+ 
+   printf("Enter the location where you wish to insert an element\n");
+   scanf("%d", &position);
+ 
+   printf("Enter the value to insert\n");
+   scanf("%d", &value);
+ 
+   for (i = n - 1; i >= position - 1; i--)
+      array[i+1] = array[i];
+ 
+   array[position-1] = value;
+ 
+   printf("Resultant array is\n");
+ 
+   for (i = 0; i <= n; i++)
+      printf("%d\n", array[i]);
+ 
+   return 0;
+}
