32d349ecdbde192dc569028b4502938d0b46a09a
S.JOE MATHEW <53465666+joemathew-it19@users.noreply.github.com>
2019-11-07 11:28:48 +0530

    "first commit"

---
+++ swap.py
@@ -0,0 +1,8 @@
+a = float(input("enter first values:"))
+b = float(input("enter second value:"))
+print("values after swapping")
+temp = a 
+a = b
+b = temp
+print('The value of x after swapping: {}'.format(a))
+print('The value of y after swapping: {}'.format(b))
