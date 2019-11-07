ad5ca6f215386916a246b1ad69f2a4e379b4887b
S.JOE MATHEW <53465666+joemathew-it19@users.noreply.github.com>
2019-11-07 11:18:49 +0530

    "first commit"

---
+++ greatest.py
@@ -0,0 +1,9 @@
+a = float(input("enter first values:"))
+b = float(input("enter second value:"))
+c = float(input("enter third value:"))
+if (a>b) and (a>c):
+    print("a")
+elif(b>c):
+    print("b")
+else:
+    print("c")
