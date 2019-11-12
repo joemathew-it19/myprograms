9f6127169b8fd74642fba7659c617307f3c1845c
S.JOE MATHEW <53465666+joemathew-it19@users.noreply.github.com>
2019-11-12 20:49:51 +0530

    first commit

---
+++ prog20.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+void main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("the no is even");
+    }
+    else
+    {
+        printf("the no is odd");
+    }
+}
