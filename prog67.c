550f3d31d7fe7421d640da5944b863da08dcf5b6
S.JOE MATHEW <53465666+joemathew-it19@users.noreply.github.com>
2019-11-14 21:14:54 +0530

    first commit

---
+++ prog67.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void  main()
+{
+   int x,y;
+   scanf("%d %d",&x,&y);
+   if(x>y)
+   {
+       printf("x is greatest");
+   }
+   else
+   {
+       printf("y is greatest");
+   }
+}
