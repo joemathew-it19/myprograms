91d093a8816cd35a71345c59a98aabc16f574ed3
S.JOE MATHEW <53465666+joemathew-it19@users.noreply.github.com>
2019-11-14 11:56:31 +0530

    first commit

---
+++ fun_avg.py
@@ -0,0 +1,16 @@
+def find_total(a,b):
+    return a+b;
+def find_avg(a,b):
+    return (a+b)/2;
+        
+
+
+a = int(input("enter first value"))
+b = int(input("enter second value"))
+total = find_total(a,b)
+print("the total",total)
+average = find_avg(a,b)
+print("the average",average)
+
+
+
