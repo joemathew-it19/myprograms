f0f5922aa6d82f8c0be0e96d7319dfb5d4e37c97
S.JOE MATHEW <53465666+joemathew-it19@users.noreply.github.com>
2019-11-15 20:49:12 +0530

    first commit

---
+++ prog15.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+   for(i=0;i<10;i++)
+   {
+        scanf("%d",&n);
+         if(n==23)
+   {
+       printf("it is equal to 23");
+       break;
+   }
+   else
+   {
+       printf("not equal");
+   }
+    
+   }
+  
+    return 0;
+}
