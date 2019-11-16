bb278642ac31aaca77429e2589e1cb2263a77dff
S.JOE MATHEW <53465666+joemathew-it19@users.noreply.github.com>
2019-11-16 15:09:37 +0530

    first commit

---
+++ append_insert_pop.py
@@ -0,0 +1,14 @@
+data = [1,2]
+print(data)
+data.append(2)
+print(data)
+data.insert(1,5)
+print(data)
+for i in range(0,3):
+    x=input()
+    data.append(x)
+print(data)
+data.pop(3)
+print(data)
+data.remove(9)
+print(data)
